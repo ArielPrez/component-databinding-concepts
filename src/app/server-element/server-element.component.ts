db93558e9f0f716b57e83b8600fe3756bc5097d8
Ariel Perez <ariel@Ariels-MacBook-Pro.local>
2020-08-22 12:00:31 -0400

    DoCheck,

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit, Input, ViewEncapsulation, SimpleChanges, OnChanges } from '@angular/core';
+import { Component,
+  OnInit,
+  Input,
+  ViewEncapsulation,
+  SimpleChanges,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -8,7 +19,14 @@ import { Component, OnInit, Input, ViewEncapsulation, SimpleChanges, OnChanges }
   // To ensure only this component receives the style defines in it,
     // in browsers that don't support the Angular view encapsulation.
 })
-export class ServerElementComponent implements OnInit, OnChanges {
+export class ServerElementComponent implements
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked {
   // The INPUT Decorator have the type of data to be show.
   // Need it to send the data from the parent component to the child component.
   // tslint:disable-next-line: no-input-rename
@@ -26,5 +44,23 @@ export class ServerElementComponent implements OnInit, OnChanges {
   }
 
   ngOnInit(): void { console.log('ngOnInit called!'); }
+
+  // tslint:disable-next-line: use-life-cycle-interface
+  ngDoCheck(): void {
+    // Called every time that the input properties of a component or a directive are checked.
+    // Use it to extend change detection by performing a custom check.
+    // Add 'implements DoCheck' to the class.
+    console.log('ngDoCheck called!');
+  }
+  ngAfterContentInit(): void { console.log('ngAfterContentInit called!'); }
+
+  ngAfterContentChecked(): void {
+    // Called after every check of the component's or directive's content.
+    // Add 'implements AfterContentChecked' to the class.
+    console.log('ngAfterContentChecked called!');
+  }
+  ngAfterViewInit(): void { console.log('ngAfterViewInit called!'); }
+
+  ngAfterViewChecked(): void { console.log('ngAfterViewChecked called!'); }
 
 }
